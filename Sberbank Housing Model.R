74a100ecdfd0991eb874db7c3b102d45cd30a2ef
pvaiciunas <p.l.vaiciunas@gmail.com>
2017-05-08 22:10:02 -0400

    Change factors to integers (might not be the best idea)
    Fill in missing values with median values

--- Sberbank Housing Model.R
+++ Sberbank Housing Model.R
@@ -17,7 +17,33 @@ full_train <- full_train[colSums(is.na(full_train)) < nrow(full_train)/2]
 # Use those same factors in the test set
 full_test <- full_test[, which(names(full_test) %in% names(full_train))]
 
-# fill in the missing values with the median value
+# Change factors to integers
+for(i in 1:ncol(full_train)){
+  if(is.factor(full_train[,i])) {
+    full_train[,i] <- as.integer(full_train[,i])
+  }
+}
+
+for(i in 1:ncol(full_test)){
+  if(is.factor(full_test[,i])) {
+    full_test[,i] <- as.integer(full_test[,i])
+  }
+}
+
+# Fill in the missing data with median
+# This might make the sub-area factor useless. Might want to remove.
+for(i in 1:ncol(full_train)){
+  full_train[is.na(full_train[,i]), i] <- median(full_train[,i], na.rm = TRUE)
+}
+
+for(i in 1:ncol(full_test)){
+  full_test[is.na(full_test[,i]), i] <- median(full_test[,i], na.rm = TRUE)
+}
+
+
+
+
+
 
 # Select the factors you want to use. Expand this later.
 factors <- c("price_doc",
